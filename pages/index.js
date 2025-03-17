ab28dc2fdf72dbfc608d1e77cb67682fc264580b
gpereira013 <gabrielfper13@gmail.com>
2025-03-17 20:33:27 -0300

    index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 
 export default function GoalAnalysis() {
   const [team1, setTeam1] = useState("");
